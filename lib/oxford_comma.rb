98075c446ce0d71d4050822088961e4a25c5d1d1
github username <github email address>
2017-09-05 15:36:50 -0400

    Finished the oxford comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.count == 0
+  elsif array.count == 1
+    array[0]
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last_element = array.delete_at(-1)
+    formatted_sentence = array.join(", ")
+    formatted_sentence << ", and #{last_element}"
+    formatted_sentence
+  end
 end
